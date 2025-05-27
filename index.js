c8133d2ad4c7d97d735381a8f3bc5d9f654a54b7
DarkKnight-404 <pshychicexcusealpha@gmail.com>
2025-05-27 13:33:47 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,67 @@
+let express = require("express");
+let app = express();
+const fs = require("fs");
+const path = require("path");
+
+app.use(express.static("C:\\Users\\irg\\OneDrive\\Desktop\\remote_terminal\\public"));
+app.use(express.json());
+
+
+
+
+async function generateProjectImage(dirName) {
+    let fileArr;
+    let completeFileArr;
+
+    fileArr = await fs.promises.readdir(__dirname, { withFileTypes: true })
+    return {
+        fileArr
+    }
+
+}
+generateProjectImage().then(val => {
+    let arr = val.fileArr.map((dirent) => {
+        return {
+            name: dirent.name,
+            parentPath: dirent.parentPath,
+            path: path.join(dirent.path, dirent.name)
+        }
+    })
+    console.log(arr)
+})
+
+
+
+
+app.post("/projDirs", (req, res) => {
+    console.log(req.body);
+    generateProjectImage().then(val => {
+        let arr = val.fileArr.map((dirent) => {
+            return {
+                name: dirent.name,
+                parentPath: dirent.parentPath.replace(/\\/g,"\\\\"),
+                path: path.join(dirent.path, dirent.name).replace(/\\/g,"\\\\")
+            }
+        })
+        console.log(arr);
+        res.send(JSON.stringify({
+            fileArr: arr
+        }));
+    })
+})
+
+
+app.post("/", (req, res) => {
+
+    console.log(req.body);
+    try {
+
+        console.log("pass")
+        const rs = fs.createReadStream(req.body.fileName)
+        rs.pipe(res);
+    } catch (error) {
+        // console.log(error)
+    }
+})
+
+app.listen(9000, () => { console.log("launched") });
